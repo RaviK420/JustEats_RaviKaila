dab150e1be72ca18d6f918fe2996b2cc6d66019f
RaviK420 <raviskaila@gmail.com>
2025-03-29 21:40:07 +0000

    Changed recyclerView's look to be more appealing to the user.

--- app/src/main/java/com/example/justeats_project_ravikaila/MainActivity.kt
+++ app/src/main/java/com/example/justeats_project_ravikaila/MainActivity.kt
@@ -1,13 +1,18 @@
 package com.example.justeats_project_ravikaila
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.TextView
+import android.Manifest
+import android.util.Log
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.justeats_project_ravikaila.API_Models.APIResponse
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,6 +31,7 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(this@MainActivity,"Fetched ${response.restaurants.size}",Toast.LENGTH_SHORT).show()
                 val adapter = RestaurantAdapter(response.restaurants)
                 recyclerView.adapter = adapter
+                println(response.restaurants)
 
             }
 
--- app/src/main/java/com/example/justeats_project_ravikaila/RestaurantAdapter.kt
+++ app/src/main/java/com/example/justeats_project_ravikaila/RestaurantAdapter.kt
@@ -6,11 +6,15 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import org.w3c.dom.Text
 
 class RestaurantAdapter(private val restaurant: List<Restaurant>): RecyclerView.Adapter<RestaurantAdapter.RestaurantViewHolder>() {
 
     inner class RestaurantViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
-        val textView: TextView = itemView.findViewById(R.id.restaurantText)
+        val restaurantName: TextView = itemView.findViewById(R.id.restaurantName)
+        val location: TextView = itemView.findViewById(R.id.restaurantLocation)
+        val cuisines: TextView = itemView.findViewById(R.id.restaurantCuisines)
+        val restaurantRating :TextView = itemView.findViewById(R.id.restaurantRating)
     }
     override fun onCreateViewHolder(parent:ViewGroup,viewType: Int): RestaurantViewHolder{
         val view = LayoutInflater.from(parent.context)
@@ -20,7 +24,11 @@ class RestaurantAdapter(private val restaurant: List<Restaurant>): RecyclerView.
 
     override fun onBindViewHolder(holder: RestaurantViewHolder, position: Int) {
         val restaurant = restaurant[position]
-        holder.textView.text = "${restaurant.name} is located at ${restaurant.address.firstLine}, ${restaurant.address.postalCode} has a score of ${restaurant.rating.starRating}, and its cuisines are (add cuisines)"
+        holder.restaurantName.text = restaurant.name
+        holder.location.text = "${restaurant.address.firstLine}, ${restaurant.address.postalCode}"
+        holder.cuisines.text = restaurant.cuisines.joinToString(", ") { it.name }
+        holder.restaurantRating.text = "${restaurant.rating.starRating.toString()} *"
+//        holder.textView.text = "${restaurant.name} is located at ${restaurant.address.firstLine}, ${restaurant.address.postalCode} has a score of ${restaurant.rating.starRating}, and its cuisines are ${restaurant.cuisines}"
 
     }
 
